2669395a3d6ad08126e09ca9a54a49d96ee61c82
Tu Nombre <tu.email@example.com>
2026-03-19 22:20:57 -0500

    tipoventas3

---
+++ Migrations/20260320032001_Tipoventas.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BarberiaApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Tipoventas : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ClienteNombre",
+                table: "Ventas",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "TipoVenta",
+                table: "Ventas",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: true,
+                defaultValue: "Venta Invitado");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ClienteNombre",
+                table: "Ventas");
+
+            migrationBuilder.DropColumn(
+                name: "TipoVenta",
+                table: "Ventas");
+        }
+    }
+}
